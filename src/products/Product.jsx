af0d2122f444db31bcbc6cca5666405f4414c2a0
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-02-09 10:57:57 +0100

    updated

--- src/products/Product.jsx
+++ src/products/Product.jsx
@@ -3,24 +3,25 @@ import "./Product.css";
 
 const Product = () => {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>Title</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>30</strong>
-          </p>
-          <div className="product_rating">☆☆☆</div>
-        </div>
-
-        <img
-          src="https://images.unsplash.com/photo-1603219527847-24c87f552a77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGJhZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
-          alt=""
-          className="product-image"
-        />
-        <button>Add to Basket</button>
+    <div className="product">
+      <div className="product__info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          deleniti!
+        </p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>30</strong>
+        </p>
+        <div className="product_rating">☆☆☆</div>
       </div>
+
+      <img
+        src="https://images.unsplash.com/photo-1603219527847-24c87f552a77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGJhZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
+        alt=""
+        className="product-image"
+      />
+      <button>Add to Basket</button>
     </div>
   );
 };
