63ab5c96da632b090e8ad8b5d3b39756f9ad6e03
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-02-13 01:35:54 +0100

    form component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
-import Contacts from "./components/contacts";
+import { useState, useEffect } from "react";
+import Form from "./components/contacts/Form";
+
+import List from "./components/contacts/List";
 
 function App() {
+
+  const [contacts, setContacts] =useState([]);
+
+  useEffect(()=>{
+    console.log(contacts)
+  },[contacts])
+
   return (
     <div className="App">
-     <Contacts/>
+ 
+     <List/>
+     <Form addContact={setContacts} contacts={contacts}/>
     </div>
   );
 }
---
+++ src/components/contacts/Form/index.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+const initialFormValues = { fullname: "", phone_number: "" };
+
+const Form = ({ addContact, contacts }) => {
+  const [form, setForm] = useState(initialFormValues);
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  useEffect(() => {
+    setForm(initialFormValues);
+  }, [contacts]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+
+    addContact([...contacts, form]);
+    console.log(form);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          type="text"
+          placeholder="Full Name"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
+      </div>
+
+      <div>
+        <input
+          name="phone_number"
+          type="text"
+          placeholder="Phone Number"
+          value={form.phone_number}
+          onChange={onChangeInput}
+        />
+      </div>
+
+      <div className="">
+        <button>Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
