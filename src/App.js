2c58f05165f3ffaceecc973af2c58f29fd9e35ae
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-02-14 16:35:19 +0100

    filtered fonk. done

--- src/App.js
+++ src/App.js
@@ -8,14 +8,28 @@ import List from "./components/contacts/List";
 
 function App() {
 
-  const [contacts, setContacts] =useState([]);
+  const [contacts, setContacts] =useState([
+    {
+    fullname:"Kerim",
+    phone_number:"029273"
+    },
+    {
+    fullname:"Ayse",
+    phone_number:"124123"
+    }
+    ,
+    {
+    fullname:"Damla",
+    phone_number:"937464"
+    }
+]);
 
   useEffect(()=>{
     console.log(contacts)
   },[contacts])
 
   return (
-    <div className="App">
+    <div>
  
      <List contacts={contacts}/>
      <Form addContact={setContacts} contacts={contacts}/>
