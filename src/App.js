3536e48befabf5eab7cf38d442265ce98027cb0c
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-02-27 11:17:44 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
-import "./App.css";
+import React from 'react'
+import ReactDOM from "react-dom/client";
+ 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-import { useState, useEffect } from "react";
-
-import Form from "./components/contacts/Form";
-
-import List from "./components/contacts/List";
-import "/Users/kerimgurbaz/Desktop/my-react-projects/eshop/src/components/contacts/styles.css"
-import User from "./components/User";
-
-function App() {
-
-  const [contacts, setContacts] =useState([
-    {
-    fullname:"Kerim",
-    phone_number:"029273"
-    },
-    {
-    fullname:"Ayse",
-    phone_number:"124123"
-    }
-    ,
-    {
-    fullname:"Damla",
-    phone_number:"937464"
-    }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
 ]);
 
-  useEffect(()=>{
-    console.log(contacts)
-  },[contacts])
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+const App = () => {
   return (
-    <div className="App">
-<div id="container">
-  <h1>Contacts</h1>
-      <User/>
-      <br />
-     <List contacts={contacts} />
-     <Form addContact={setContacts} contacts={contacts}/>
-</div>
+    <div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
